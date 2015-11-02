3dd20aadd15e07e495bf9745308e3a6212193ce3
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-11-02 20:19:36 +0100

    add function for init data dir, downloading and extracting compressed geojson

---
+++ exporter/utility/utils.py
@@ -0,0 +1,29 @@
+import logging
+LOG = logging.getLogger(__file__)
+
+import os
+import wgetter
+import tarfile
+import shutil
+
+
+def init_path(full_path):
+    """
+    Initializes full path and recreate if already exists
+    """
+    dir_path = os.path.dirname(full_path)
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(dir_path)
+
+
+def download(url, output_dir):
+    filename = wgetter.download(url, outdir=output_dir)
+    if filename:
+        LOG.info('Downloaded file is at %s ...', filename)
+    return filename
+
+
+def extract(filename, output_dir):
+    with tarfile.open(filename) as f:
+        f.extractall(path=output_dir)

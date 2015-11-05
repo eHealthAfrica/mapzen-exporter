87ab437a6db05bfeb0f49a9c6b353c35d3f74b5b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-11-05 19:20:20 +0100

    create exporter program entry point

---
+++ exporter/exporter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from settings.mapzen_settings import MapZenSettings
+from utility import utils
+from parser import process
+
+argparser = argparse.ArgumentParser(
+    description='Exports given MapZen extracts .zip file'
+)
+
+argparser.add_argument(
+    '--settings', default='settings.yaml',
+    help='path to the settings file, default: settings.yaml'
+)
+
+if __name__ == '__main__':
+    args = argparser.parse_args()
+    config = MapZenSettings(args.settings)
+    settings = config.get_settings()
+
+    url = settings.get('sources').get('mapzen_file_url')
+    dest = settings.get('sources').get('data_dir')
+    extracted_geojson_dir = settings.get('sources').get('geojson_dir_name')
+    admin_levels = sorted(settings.get('admin_levels'))
+
+    #utils.init_path(dest)
+    #file_path = utils.download(url, dest)
+    #utils.extract(file_path, dest)
+    #process.parse(extracted_geojson_dir, admin_levels)
+
+    #TODO: uncomment lines above and decide actions to take base on args passed
+
